eb682fbd7c2fc95707ba7034aaf392caaf024cce
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2022-12-05 23:24:45 -0300

    feature - cadastro de marca

--- src/pages/Menu/components/Marcas/hooks/useMenuMarcas.ts
+++ src/pages/Menu/components/Marcas/hooks/useMenuMarcas.ts
@@ -1,46 +1,63 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import MarcasApi from "../../../../../api/Marcas";
-import { Marcas, IColumns, IPageRequest, IActionButtons } from "../../../../../types/common.types";
+import {
+  Marcas,
+  IColumns,
+  IPageRequest,
+  IActionButtons,
+} from "../../../../../types/common.types";
 
 const useMenuMarcas = () => {
+  const { listMarcas } = MarcasApi();
+  const navigate = useNavigate();
+  const actions: IActionButtons = {
+    view: false,
+    edit: false,
+    disable: true,
+  };
+  const [pageable, setPageable] = useState<IPageRequest>({
+    filter: "",
+    pageSize: 3,
+    currentPage: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(false);
+  const [list, setList] = useState<Marcas[]>([]);
+  const [isToAddNewMarca, setIsToAddNewMarca] = useState<boolean>(false);
+  const [newMarca, setNewMarca] = useState<string>("");
 
-    const { listMarcas } = MarcasApi();
-    const navigate = useNavigate();
-    const actions: IActionButtons = {
-        view: false,
-        edit: false,
-        disable: true,
-    };
-    const [pageable, setPageable] = useState<IPageRequest>(
-        {
-            filter: '',
-            pageSize: 3,
-            currentPage: 0,
-        }
-    );
-    const [loading, setLoading] = useState<boolean>(false);
-    const [hasMore, setHasMore] = useState<boolean>(false);
-    const [list, setList] = useState<Marcas[]>([]);
-    const columns:IColumns[] = [
-        {
-            title: 'Nome',
-            name: 'nome',
-        },
-    ];
+  const columns: IColumns[] = [
+    {
+      title: "Nome",
+      name: "nome",
+    },
+  ];
 
-    useEffect(() => {
-        listMarcas(pageable)
-            .then((response) => {
-                setHasMore(response.content.length > 0)
-                return setList([...list, ...response.content]);   
-            })
-            .finally (() => {
-                return setLoading(false);
-            });
-    }, [pageable]);
+  useEffect(() => {
+    listMarcas(pageable)
+      .then((response) => {
+        setHasMore(response.content.length > 0);
+        return setList([...list, ...response.content]);
+      })
+      .finally(() => {
+        return setLoading(false);
+      });
+  }, [pageable]);
 
-    return { actions, columns, list,loading, hasMore, setPageable, navigate };
+  return {
+    actions,
+    columns,
+    list,
+    loading,
+    hasMore,
+    setPageable,
+    navigate,
+    isToAddNewMarca,
+    setIsToAddNewMarca,
+    newMarca,
+    setNewMarca
+  };
 };
 
 export default useMenuMarcas;
--- src/pages/Menu/components/Marcas/index.tsx
+++ src/pages/Menu/components/Marcas/index.tsx
@@ -1,19 +1,33 @@
-import * as S from './styles';
-import List from '../../../../components/List';
-import ButtonInsert from '../../../../components/ButtonInsert';
-import useMenuMarcas from './hooks/useMenuMarcas';
+import * as S from "./styles";
+import List from "../../../../components/List";
+import ButtonInsert from "../../../../components/ButtonInsert";
+import useMenuMarcas from "./hooks/useMenuMarcas";
+import { Endpoints } from "../../../../components/SmallForm/types";
+import { SelectFieldLabels } from "../../../../components/Modal/Components/SelectField/selectfield.types";
+import SmallForm from "../../../../components/SmallForm";
 
 const Marcas = () => {
-
-  const { 
+  const {
     actions,
-    columns, 
-    list, 
+    columns,
+    list,
     loading,
-    hasMore, 
-    setPageable, 
-    navigate 
+    hasMore,
+    setPageable,
+    navigate,
+    isToAddNewMarca,
+    setIsToAddNewMarca,
+    newMarca,
+    setNewMarca,
   } = useMenuMarcas();
+
+  const showFormNewModelo = (isToAdd: boolean) => {
+    if (!isToAdd) {
+      setIsToAddNewMarca(isToAdd);
+      setNewMarca("");
+    }
+    setIsToAddNewMarca(isToAdd);
+  };
 
   return (
     <>
@@ -21,30 +35,36 @@ const Marcas = () => {
         <S.StackHeader>
           <S.Title>Marcas</S.Title>
           <S.Actions>
-            <ButtonInsert
-              onClick={() => navigate('new')}
-            >
+            <ButtonInsert onClick={() => showFormNewModelo(true)}>
               Cadastrar Marca
             </ButtonInsert>
           </S.Actions>
         </S.StackHeader>
+        {isToAddNewMarca ? (
+          <SmallForm
+            value={newMarca}
+            setValue={setNewMarca}
+            hideForm={setIsToAddNewMarca}
+            label={SelectFieldLabels.MARCA}
+            endpoint={Endpoints.MARCA}
+          />
+        ) : null}
         <S.ListArea>
-          {
-            list.length > 0 ?
-              <List 
-                actions={actions}
-                loading={loading}
-                hasMore={hasMore} 
-                setNewPage={setPageable} 
-                columns={columns} 
-                items={list} 
-                mode={'list'} 
-              /> : null
-          }
+          {list.length > 0 ? (
+            <List
+              actions={actions}
+              loading={loading}
+              hasMore={hasMore}
+              setNewPage={setPageable}
+              columns={columns}
+              items={list}
+              mode={"list"}
+            />
+          ) : null}
         </S.ListArea>
       </S.Container>
     </>
   );
-}
+};
 
 export default Marcas;
