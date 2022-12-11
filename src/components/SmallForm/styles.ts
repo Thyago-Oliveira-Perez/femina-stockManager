0d3a966a6bf26c13a66e790d696efabbd2695f07
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2022-12-11 20:30:10 -0300

    changed - ajuste no tamanho do smallform

--- src/components/SmallForm/styles.ts
+++ src/components/SmallForm/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AddArea = styled.div`
   width: 80%;
-  height: 100%;
+  max-height: 360px;
   display: flex;
   align-items: center;
   justify-content: space-around;
